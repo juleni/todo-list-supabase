10f42c6a7a5db5644e6281f6a1e97ff526ecea0d
juleni <69912179+juleni@users.noreply.github.com>
2023-06-27 10:06:38 +0200

    Added FiTrash2 icon

--- src/components/DeleteTask.js
+++ src/components/DeleteTask.js
@@ -1,5 +1,6 @@
 import { IconButton } from '@chakra-ui/react';
+import { FiTrash2 } from 'react-icons/fi';
 
 export default function DeleteTask({ id }) {
-  return <IconButton isRound="true" />;
+  return <IconButton isRound="true" icon={<FiTrash2 />} />;
 }
