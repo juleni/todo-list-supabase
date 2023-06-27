efe8cc4c0bd1bb1c006bd707c9590e5b5e42e53c
juleni <69912179+juleni@users.noreply.github.com>
2023-06-27 15:13:57 +0200

    Added supabase impl for task deleting

--- src/components/DeleteTask.js
+++ src/components/DeleteTask.js
@@ -1,6 +1,29 @@
 import { IconButton } from '@chakra-ui/react';
+import { useState } from 'react';
 import { FiTrash2 } from 'react-icons/fi';
+import supabaseClient from '../supabaseClient';
 
-export default function DeleteTask({ id }) {
-  return <IconButton isRound="true" icon={<FiTrash2 />} />;
+export default function DeleteTask({ id, tasks, setTasks }) {
+  const [loading, setLoading] = useState(false);
+
+  async function handleClick() {
+    setLoading(true);
+    const { data, error } = await supabaseClient
+      .from('todo')
+      .delete()
+      .eq('id', id);
+
+    // delete task from array prop
+    const newTasks = tasks.filter(item => item.id !== id);
+    setTasks(newTasks);
+    setLoading(false);
+  }
+  return (
+    <IconButton
+      onClick={handleClick}
+      isLoading={loading}
+      isRound="true"
+      icon={<FiTrash2 />}
+    />
+  );
 }
