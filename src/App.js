e45ab297f2abba87e0ddfec9b85eed13c497e067
juleni <69912179+juleni@users.noreply.github.com>
2023-06-27 10:45:58 +0200

    Added content

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {
   ChakraProvider,
   Grid,
   Heading,
+  HStack,
+  Text,
   theme,
   VStack,
 } from '@chakra-ui/react';
@@ -14,24 +16,51 @@ import TaskList from './components/TaskList';
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box fontSize="xl">
+      <Box fontSize="xl" borderWidth="4px" m="5" pb="10" borderRadius="lg">
         <Grid p={5}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={2} minH="100vh">
+          <HStack>
+            <Text
+              w="100%"
+              textAlign="right"
+              color="gray.500"
+              fontSize={{
+                base: 'sm',
+                sm: 'sm',
+                lg: 'lg',
+                xl: 'xl',
+              }}
+            >
+              Powered by Chakra UI & Supabase
+            </Text>
+            <ColorModeSwitcher justifySelf="flex-end" />
+          </HStack>
+          <VStack spacing={2}>
             <Heading
-              mt="5"
               p="5"
               fontWeight="extrabold"
               size="xl"
               bgGradient="linear(to-l), teal.300, blue.500"
-              bgClip="text"
+              color="blue.500"
             >
-              TODO List
+              TODO List SupaBASE
             </Heading>
             <AddTask />
             <TaskList />
           </VStack>
         </Grid>
+        <Text
+          w="100%"
+          color="gray.400"
+          pl="5"
+          fontSize={{
+            base: 'sm',
+            sm: 'sm',
+            lg: 'lg',
+            xl: 'xl',
+          }}
+        >
+          JULENI 2023 ... See my GitHub pages
+        </Text>
       </Box>
     </ChakraProvider>
   );
