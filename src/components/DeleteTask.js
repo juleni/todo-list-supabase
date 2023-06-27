485d7bd9f61475c42dcd83acc1d0bfcc7c10dc10
juleni <69912179+juleni@users.noreply.github.com>
2023-06-27 16:02:33 +0200

    Added toast message after delete

--- src/components/DeleteTask.js
+++ src/components/DeleteTask.js
@@ -1,10 +1,11 @@
-import { IconButton } from '@chakra-ui/react';
+import { IconButton, useToast } from '@chakra-ui/react';
 import { useState } from 'react';
 import { FiTrash2 } from 'react-icons/fi';
 import supabaseClient from '../supabaseClient';
 
 export default function DeleteTask({ id, tasks, setTasks }) {
   const [loading, setLoading] = useState(false);
+  const toast = useToast();
 
   async function handleClick() {
     setLoading(true);
@@ -17,6 +18,14 @@ export default function DeleteTask({ id, tasks, setTasks }) {
     const newTasks = tasks.filter(item => item.id !== id);
     setTasks(newTasks);
     setLoading(false);
+
+    toast({
+      title: error || 'TODO Task deleted.',
+      status: error ? 'error' : 'success',
+      position: 'top-left',
+      duration: 2000,
+      isClosable: true,
+    });
   }
   return (
     <IconButton
