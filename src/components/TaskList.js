77f12cdd063763cb98773630017d833902f92660
juleni <69912179+juleni@users.noreply.github.com>
2023-06-27 15:14:23 +0200

    Added supabase impl for listing tasks

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,42 +1,63 @@
-import { Box, Image, StackDivider, VStack } from '@chakra-ui/react';
-//import { ReactComponent as Logo } from '../images/todolist.svg';
+import {
+  Box,
+  HStack,
+  Image,
+  StackDivider,
+  Text,
+  VStack,
+} from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import img from '../images/todolist.svg';
+import supabaseClient from '../supabaseClient';
 import ClearTasks from './ClearTasks';
+import DeleteTask from './DeleteTask';
 
 export default function TaskList() {
-  return (
-    <Box align="center">
-      <Image
-        src={img}
-        maxW={{ base: '60%', sm: '60%', lg: '40%', xl: '20%' }}
-      />
-    </Box>
-  );
+  const [tasks, setTasks] = useState([]);
 
-  return (
-    <>
-      <VStack
-        divider={<StackDivider />}
-        borderColor="gray.100"
-        borderWidth="2px"
-        p="5"
-        borderRadius="lg"
-        w="100%"
-        maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '30vw' }}
-        alignItems="stretch"
-        shadow="xl"
-      >
-        {/**
-         * 
-        <HStack key="">
-          <Text w="100%" p="8px" borderRadius="lg">
-            To walk the dog
-          </Text>
-          <DeleteTask />
-        </HStack>
-         */}
-      </VStack>
-      <ClearTasks />
-    </>
-  );
+  async function fetchData() {
+    let { data: tasks, error } = await supabaseClient.from('todo').select('*');
+    setTasks(tasks);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!tasks.length) {
+    return (
+      <Box align="center">
+        <Image
+          src={img}
+          maxW={{ base: '60%', sm: '60%', lg: '40%', xl: '20%' }}
+        />
+      </Box>
+    );
+  } else {
+    return (
+      <>
+        <VStack
+          divider={<StackDivider />}
+          borderColor="gray.100"
+          borderWidth="2px"
+          p="5"
+          borderRadius="lg"
+          w="100%"
+          maxW={{ base: '90vw', sm: '80vw', lg: '50vw', xl: '30vw' }}
+          alignItems="stretch"
+          shadow="xl"
+        >
+          {tasks.map(task => (
+            <HStack key={task.id}>
+              <Text w="100%" p="8px" borderRadius="lg">
+                {task.text}
+              </Text>
+              <DeleteTask id={task.id} tasks={tasks} setTasks={setTasks} />
+            </HStack>
+          ))}
+        </VStack>
+        <ClearTasks />
+      </>
+    );
+  }
 }
