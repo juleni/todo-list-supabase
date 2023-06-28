810f61781712edda421849f46231bde5fd007cb3
juleni <69912179+juleni@users.noreply.github.com>
2023-06-28 13:45:11 +0200

    Added functionality for deleting all data

--- src/components/ClearTasks.js
+++ src/components/ClearTasks.js
@@ -1,6 +1,29 @@
-import { Button, Flex } from '@chakra-ui/react';
+import { Button, Flex, useToast } from '@chakra-ui/react';
+import { useState } from 'react';
+import supabaseClient from '../supabaseClient';
 
-export default function ClearTasks() {
+export default function ClearTasks({ setReloadList }) {
+  const [loading, setLoading] = useState(false);
+  const toast = useToast();
+
+  async function handleClick(e) {
+    // Delete all tasks from db
+    setLoading(true);
+    const { data, error } = await supabaseClient
+      .from('todo')
+      .delete()
+      .neq('id', 0);
+    toast({
+      title: error || 'All TODO tasks were deleted.',
+      status: error ? 'error' : 'success',
+      position: 'top-left',
+      duration: 2000,
+      isClosable: true,
+    });
+    setReloadList(true);
+    setLoading(false);
+  }
+
   return (
     <Flex>
       <Button
@@ -10,6 +33,9 @@ export default function ClearTasks() {
         h="45"
         color="gray.500"
         mt="10"
+        onClick={handleClick}
+        isLoading={loading}
+        loadingText="Clearing ..."
       >
         Clear Tasks
       </Button>
