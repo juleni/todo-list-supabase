a98244c892df3dcb6b213feff9ab880a0ab0047a
juleni <69912179+juleni@users.noreply.github.com>
2023-07-03 17:37:36 +0200

    Added simple delete password protection

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,9 +1,21 @@
 import {
   Box,
+  Button,
   HStack,
   Image,
-  StackDivider,
+  Input,
+  InputGroup,
+  InputRightElement,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
   Text,
+  useDisclosure,
+  useToast,
   VStack,
 } from '@chakra-ui/react';
 import { useEffect, useState } from 'react';
@@ -15,12 +27,71 @@ import DeleteTask from './DeleteTask';
 export default function TaskList({ reloadList, setReloadList }) {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [show, setShow] = useState(false);
+  const handlePClick = () => setShow(!show);
+  const [value, setValue] = useState('');
+  const [deleteItem, setDeleteItem] = useState(null);
+  const toast = useToast();
 
   async function fetchData() {
     setLoading(true);
     let { data: tasks, error } = await supabaseClient.from('todo').select('*');
     setTasks(tasks);
     setLoading(false);
+  }
+
+  function handleDeleteTask() {
+    onOpen();
+  }
+
+  const handleChangeValue = event => setValue(event.target.value);
+
+  async function handleCheckValue(e) {
+    e.preventDefault();
+    console.log('handleCheckValue');
+    if (value === 'todo') {
+      onClose();
+      // Delete task from db
+      let errMessage = null;
+      let title = '';
+
+      if (deleteItem === -1) {
+        title = 'All TODO tasks were deleted.';
+        // Delete all tasks from db
+        const { data, error } = await supabaseClient
+          .from('todo')
+          .delete()
+          .neq('id', 0);
+        errMessage = error;
+      } else if (deleteItem > 0) {
+        // Delete one specific task from db
+        title = 'TODO Task deleted.';
+        const { data, error } = await supabaseClient
+          .from('todo')
+          .delete()
+          .eq('id', deleteItem);
+
+        errMessage = error;
+        // delete task from array prop
+        const newTasks = tasks.filter(item => item.id !== deleteItem);
+        setTasks(newTasks);
+      }
+
+      toast({
+        title: errMessage || title,
+        status: errMessage ? 'error' : 'success',
+        position: 'top-left',
+        duration: 2000,
+        isClosable: true,
+      });
+
+      setReloadList(true);
+    } else {
+      console.log('WRONG PASSWORD');
+      onClose();
+    }
+    setValue('');
   }
 
   useEffect(() => {
@@ -54,7 +125,6 @@ export default function TaskList({ reloadList, setReloadList }) {
     return (
       <>
         <VStack
-          divider={<StackDivider />}
           borderColor="gray.100"
           borderWidth="2px"
           p="5"
@@ -64,16 +134,66 @@ export default function TaskList({ reloadList, setReloadList }) {
           alignItems="stretch"
           shadow="xl"
         >
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>Delete record check</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <InputGroup size="md">
+                  <Input
+                    pr="4.5rem"
+                    type={show ? 'text' : 'password'}
+                    placeholder="Enter password"
+                    onChange={handleChangeValue}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={handlePClick}>
+                      {show ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button
+                  type="submit"
+                  shadow="xl"
+                  colorScheme="pink"
+                  px="20px"
+                  py="9px"
+                  h="100%"
+                  mr="8px"
+                  onClick={e => handleCheckValue(e)}
+                  isLoading={loading}
+                  loadingText="Closing"
+                >
+                  Check
+                </Button>
+                <Button onClick={onClose}>Cancel</Button>
+              </ModalFooter>
+            </ModalContent>
+          </Modal>
+          ;
           {tasks.map(task => (
             <HStack key={task.id}>
               <Text w="100%" p="8px" borderRadius="lg">
                 {task.text}
               </Text>
-              <DeleteTask id={task.id} tasks={tasks} setTasks={setTasks} />
+              <DeleteTask
+                id={task.id}
+                tasks={tasks}
+                setTasks={setTasks}
+                handleDeleteTask={handleDeleteTask}
+                setDeleteItem={setDeleteItem}
+              />
             </HStack>
           ))}
         </VStack>
-        <ClearTasks setReloadList={setReloadList} />
+        <ClearTasks
+          handleDeleteTask={handleDeleteTask}
+          setDeleteItem={setDeleteItem}
+        />
       </>
     );
   }
